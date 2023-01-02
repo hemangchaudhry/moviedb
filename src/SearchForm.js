88812fdfc642a51a7c2091efb2bdaeeb214b1208
hemangchaudhry <hemangchaudhry25@gmail.com>
2023-01-02 19:54:33 +0530

    complete

---
+++ src/SearchForm.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { useGlobalContext } from "./context"
+const SearchForm = () => {
+  const { query, setQuery, error } = useGlobalContext()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <form className="search-form" onSubmit={handleSubmit}>
+      <h2>search Movies</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {error.show && <div className="error">{error.msg}</div>}
+    </form>
+  )
+}
+
+export default SearchForm
